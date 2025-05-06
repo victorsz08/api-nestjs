52281982e0cdae43927033ed4b2398b2acc58501
Victor Siqueira <131918226+victorsz08@users.noreply.github.com>
2025-05-06 22:24:27 +0000

    fix

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({}));
+  app.use(cookieParser());
 
   await app.listen(process.env.PORT ?? 3000);
 };
